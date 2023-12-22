503e70dfbeef350f2ce77aede64ae855bf1a55da
vempuluruvk <142653652+vempuluruvk@users.noreply.github.com>
2023-12-22 10:46:06 +0530

    Add files via upload

---
+++ 21DEC.PRAC.js
@@ -0,0 +1,18 @@
+function greet(){
+    console.log("hello")
+}
+greet();
+function greet(name){
+    console.log("hello"+(name));
+}
+greet("vamsi")
+function add(a,b){
+    return a+b
+}
+let sum =add(2,3)
+console.log(sum);
+
+function sayhello(){
+    console.log("jhvunfou");
+}
+sayhello();
