19097a89aa7042c6dd5ee5e7619d30f93484078f
vempuluruvk <142653652+vempuluruvk@users.noreply.github.com>
2024-01-05 23:31:21 +0530

    Add files via upload

---
+++ 5.jan.24/loops.js
@@ -0,0 +1,80 @@
+// do while
+let result = '';
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 5);
+
+console.log(result);
+
+let results = "";
+let i = 0;
+do {
+  i += 1;
+  result += `${i} `;
+} while (i > 0 && i < 5);
+
+// continue
+let text = '';
+
+for (let i = 0; i < 10; i++) {
+  if (i === 3) {
+    continue;
+  }
+  text = text + i;
+}
+
+console.log(text);
+
+
+let i = 0;
+let n = 0;
+
+while (i < 5) {
+  i++;
+
+  if (i === 3) {
+    continue;
+  }
+
+  n += i;
+}
+
+// labeled
+const items = Array.from({ length: 100 }, (_, i) => i + 1);
+const tests = [
+  { pass: (item) => item % 2 === 0 },
+  { pass: (item) => item % 3 === 0 },
+  { pass: (item) => item % 5 === 0 },
+];
+let itemsPassed = 0;
+
+for (const item of items) {
+  let passed = true;
+  for (const test of tests) {
+    if (!test.pass(item)) {
+      passed = false;
+      break;
+    }
+  }
+  if (passed) {
+    itemsPassed++;
+  }
+}
+const items = Array.from({ length: 100 }, (_, i) => i + 1);
+const tests = [
+  { pass: (item) => item % 2 === 0 },
+  { pass: (item) => item % 3 === 0 },
+  { pass: (item) => item % 5 === 0 },
+];
+let allPass = true;
+
+itemIteration: for (const item of items) {
+  for (const test of tests) {
+    if (!test.pass(item)) {
+      allPass = false;
+      break itemIteration;
+    }
+  }
